d724248189ff8887b6e8b142601f3e76b8e45a32
cristianvlad <cristian.vlad10@gmail.com>
2022-01-08 22:11:31 +0200

    init commit

---
+++ src/main/java/com/example/springbootblogrest/SpringBootBlogRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootblogrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBlogRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBlogRestApplication.class, args);
+    }
+
+}
